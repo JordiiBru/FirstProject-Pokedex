a3828df961f94ad10fd9ffc00350596b0408e66e
jordiibru <jordibrucarci@gmail.com>
2022-07-07 21:12:06 +0200

    Understanding items and components
    2nd lesson about items modifying index.js. To see what we did, just uncomment element in the root.render.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; 
 import reportWebVitals from './reportWebVitals';
+
+//Componets are labelled with an initial capital letter
+//CARE: syntaxis
+const Item = (props) => {
+  return(
+    <div>
+      <p>This is an Item Component. It's name is {props.name}.</p>
+      <Foo name={'Kirlia'}/>
+      <Foo name={'Gardevoir'}/>
+      <Foo name={'Gallade'}/>
+      {item}
+    </div>
+  )
+}
+//Components declaration alternative version
+const Foo = ({ name }) => {
+  return(
+    <p>This is a Foo Component. It's name is {name}.</p>
+  )
+}
+
+const item = <p>This is a new item. Elements are composable.</p>
+const element = <div>
+    <h1>This is an Element!</h1>
+    <p> And this is a test :)</p>
+    <Item name={'Ralts'}/>
+  </div>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App />,
+    //element, //uncomment this line to see all changes
 );
 
 // If you want to start measuring performance in your app, pass a function
