338934c90ed4c7a1ffbcadd55c4b8860a9c2ab07
jordiibru <jordibrucarci@gmail.com>
2022-07-08 00:55:35 +0200

    Clean Start Semantic UI (def)
    Now, it is a clean start lmao

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 import App from './App'; 
 
-import { Button } from 'semantic-ui-react'
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <App />,
-
 );
 
 
