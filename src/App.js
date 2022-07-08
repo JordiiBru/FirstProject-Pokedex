771162880175c455271e45addd9d4314a06042fb
jordiibru <jordibrucarci@gmail.com>
2022-07-08 02:14:34 +0200

    Added Input form that affects the local state
    Input form + onChange events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import PokedexHeader from './Components/PokedexHeader';
+import {
+  Input,
+  Segment
+} from 'semantic-ui-react'
 import './App.css';
 
 
@@ -7,16 +11,35 @@ class App extends React.Component {
   constructor(props){ //internal state (local)
     super(props)
     this.state = {
-      name: "Jordi"
+      name: "Jordi",
+      author: "@jordi_bru"
     }
+    this.onChange = this.onChange.bind(this)
   }
+
+  onChange(event, data){
+    console.log(data.value)
+    this.setState({
+      name: data.value
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <PokedexHeader 
-          author={"@jordi_bru"} 
+          author={this.state.author} 
           name={this.state.name}
         />
+        <Segment basic>
+          <Input 
+            focus label='Input Form'
+            onChange={this.onChange}
+            value={this.state.name}
+
+          />
+        </Segment>
       </div>
     );
   }
